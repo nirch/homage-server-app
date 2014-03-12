c159fe3b131acebf9bb1dd4bb0bae90b4be60fc3
Nir Channes <nirh2@yahoo.com>
2014-03-13 00:08:43 +0200

    mongo scripts

--- mongo scripts/adhoc_mongo_script.rb
+++ mongo scripts/adhoc_mongo_script.rb
@@ -1,25 +1,64 @@
 require 'mongo'
 
-db = Mongo::MongoClient.from_uri("mongodb://Homage:homageIt12@paulo.mongohq.com:10008/Homage").db
-users = db.collection("Users")
-remakes = db.collection("Remakes")
+test_db = Mongo::MongoClient.from_uri("mongodb://Homage:homageIt12@paulo.mongohq.com:10008/Homage").db
+users = test_db.collection("Users")
+remakes = test_db.collection("Remakes")
 
-public_users_cursor = users.find({is_public:true})
-public_users = Array.new
+prod_db = Mongo::MongoClient.from_uri("mongodb://Homage:homageIt12@troup.mongohq.com:10057/Homage_Prod").db
+prod_users = prod_db.collection("Users")
+prod_remakes = prod_db.collection("Remakes")
 
-for user in public_users_cursor do
-	public_users.push(user["_id"])
+
+
+good_remakes = prod_remakes.find({demo:true})
+for remake in good_remakes do
+	user_exist = prod_users.find_one({_id:remake["user_id"]})
+	if !user_exist then
+		puts remake["_id"].to_s + "; " + remake["user_id"]
+	end
 end
 
-all_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3})
-puts "All remakes = " + all_remakes.count.to_s
-
-yoav_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3, user_id:"yoav@homage.it"})
-puts "Yoav remakes = " + yoav_remakes.count.to_s
-
-public_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3, user_id:{"$in" => public_users}})
-puts "Public remakes = " + public_remakes.count.to_s
 
 
+
+# #### Copy Remakes from Test to Prod
+
+# remakes_to_copy = remakes.find({demo:true})
+# puts "Good remakes = " + remakes_to_copy.count.to_s
+# prod_remakes = prod_db.collection("Remakes")
+
+# count = 0
+
+# for remake in remakes_to_copy do
+# 	remake_exist = prod_remakes.find_one(remake["_id"])
+# 	if !remake_exist then
+# 		count += 1
+# 		result = prod_remakes.save(remake) 
+# 		puts result
+# 	end
+# end
+
+# puts "Remakes to copy = " + count.to_s
+
+
+# #### Public Remakes #################################
+
+# public_users_cursor = users.find({is_public:true})
+# public_users = Array.new
+
+# for user in public_users_cursor do
+# 	public_users.push(user["_id"])
+# end
+
+# all_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3})
+# puts "All remakes = " + all_remakes.count.to_s
+
+# yoav_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3, user_id:"yoav@homage.it"})
+# puts "Yoav remakes = " + yoav_remakes.count.to_s
+
+# public_remakes = remakes.find({story_id: BSON::ObjectId.from_string("52de83db8bc427751c000305"), status:3, user_id:{"$in" => public_users}})
+# puts "Public remakes = " + public_remakes.count.to_s
+
+#######################################################
 
 #story_id = BSON::ObjectId.from_string("52de83db8bc427751c000305")
