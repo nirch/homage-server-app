3bef7b1bf6c58c7081076fcb9199c1012be27b5f
nirch <nirh2@yahoo.com>
2014-07-01 09:56:13 +0300

    updating user_from_remake script

--- mongo scripts/user_from_remake.rb
+++ mongo scripts/user_from_remake.rb
@@ -6,9 +6,11 @@ prod_db = Mongo::MongoClient.from_uri("mongodb://Homage:homageIt12@troup.mongohq
 prod_users = prod_db.collection("Users")
 prod_remakes = prod_db.collection("Remakes")
 
-remake_id = BSON::ObjectId.from_string("53860c720be04462e6000007")
+remake_id_string = ARGV[0]
+remake_id = BSON::ObjectId.from_string(remake_id_string)
 
 remake = prod_remakes.find_one(remake_id)
 user = prod_users.find_one(remake["user_id"])
 
+#puts remake
 puts user
