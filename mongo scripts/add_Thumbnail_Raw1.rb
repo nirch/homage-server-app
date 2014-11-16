d90094d08c2e8f53aba4fd883e3687f05b9128cc
dangalg <dangalg@gmail.com>
2014-11-16 17:41:42 +0200

    created two scripts 1 adding thumbnails to raw data base 2 downloading remakes from list

--- mongo scripts/add_Thumbnail_Raw1.rb
+++ mongo scripts/add_Thumbnail_Raw1.rb
@@ -74,7 +74,7 @@ i = 0
 # Number of failed remakes, remakes that were clicked on create movie but were not done
 #total_remakes = prod_remakes.find(created_at:{"$gte"=>launch_date})
 
-ready_remakes = prod_remakes.find({created_at:{"$gte"=>launch_date}, share_link:{"$exists"=>true}}).each do |r|
+ready_remakes = prod_remakes.find({created_at:{"$gte"=>launch_date}, status:3}).each do |r|
 	#puts ready_remakes.count
 	#, {:limit => 10}
 	#download_from_s3 (s3_key, 'D:\homage\movies')
@@ -83,6 +83,7 @@ ready_remakes = prod_remakes.find({created_at:{"$gte"=>launch_date}, share_link:
 			i = i + 1
 			log.info i
 			# create variables
+			#-----------------
 			file_name = r["_id"].to_s
 			movie_extension = ".mov"
 			thumbnail_extension = "_raw1.jpg"
@@ -90,22 +91,30 @@ ready_remakes = prod_remakes.find({created_at:{"$gte"=>launch_date}, share_link:
 			s3_key = r["footages"][0]["raw_video_s3_key"]
 			s3_upload_key = File.dirname(s3_key).to_s + "/" + file_name + thumbnail_extension
 			log.info "Creating Variables: " + "s3_key: " + s3_key + "s3_upload_key: " + s3_upload_key
+			
 			# download raw1 file from s3
+			#----------------------------
 			log.info "Start download"
 			download_from_s3(s3_key,file_path + file_name + movie_extension)
 			log.info "Finished Downloading successfully"
 			#puts r["footages"][0]["raw_video_s3_key"].to_s
-			puts r["_id"].to_s
+			#puts r["_id"].to_s
+
 			#create thumbnail of raw1
+			#------------------------
 			system "ffmpeg -ss 0 -i " + file_path + file_name + movie_extension + " -frames:v 1 -vf crop=640:360 -y " + file_path + file_name + thumbnail_extension
 			log.info "ffmpeg::::::: " + "ffmpeg -ss 0 -i " + file_path + file_name + movie_extension + " -frames:v 1 -vf crop=640:360 -y " + file_path + file_name + thumbnail_extension
 			#system "ffmpeg -y -i " + file_path + file_name + movie_extension + " -f mjpeg -ss 10 -vframes 1 640x360 " + file_path + file_name + thumbnail_extension
+			
 			#upload thumbnail of raw1 to s3
+			#--------------------------
 			log.info "Start upload"
 			upload_to_s3 file_path + file_name + thumbnail_extension, s3_upload_key, :public_read, 'image/jpeg'
 			log.info "Finished Uploading successfully"
-			#puts "file endings!!!!!!!!!!!!!!!!!!!!!!!!!" + s3_key.to_s + "    " + s3_upload_key.to_s
+			puts "file endings!!!!!!!!!!!!!!!!!!!!!!!!!" + s3_key.to_s + "    " + s3_upload_key.to_s
+			
 			#delete the movie after uploading the thumbnail
+			#------------------------------------------------
 			File.delete(file_path + file_name + movie_extension)
 			File.delete(file_path + file_name + thumbnail_extension)
 			rescue => err
