90ac5882c651c3f9928bcade1f4a97e4ec6cef49
Aviv Israel <aviv.israel@gmail.com>
2015-07-14 11:58:28 +0300

    - Share gif page analytics - Added JS file

--- emupages/shares.rb
+++ emupages/shares.rb
@@ -5,16 +5,14 @@ configure :test do
 	# Emu Beta app settings
 	set :emutest_shared_media, "http://media-test.emu.im"
 	set :emutest_share_gif_page, "http://play-test.emu.im/giftest"
-	set :emutest_img, "/emu-shares/img"
-	set :emutest_css, "/emu-shares/css"
+	set :emutest_xrc, "/emu-shares"
 	set :emutest_app_page, "http://app-test.emu.im"
 	set :emutest_fb_app_id, "428894703902516" # Emu Beta
 
 	# Emu production app settings
 	set :emu_shared_media, "http://media.emu.im"
 	set :emu_share_gif_page, "http://play.emu.im/gif"
-	set :emu_img, "/emu-shares/img"
-	set :emu_css, "/emu-shares/css"
+	set :emu_xrc, "/emu-shares"
 	set :emu_app_page, "http://app.emu.im"
 	set :emu_fb_app_id, "410672082391445" 
 end
@@ -26,16 +24,14 @@ configure :production do
 	# Emu Beta app settings
 	set :emutest_shared_media, "http://media-test.emu.im"
 	set :emutest_share_gif_page, "http://play-test.emu.im/giftest"
-	set :emutest_img, "/emu-shares/img"
-	set :emutest_css, "/emu-shares/css"
+	set :emutest_xrc, "/emu-shares"
 	set :emutest_app_page, "http://app-test.emu.im"
 	set :emutest_fb_app_id, "428894703902516" # Emu Beta
 
 	# Emu production app settings
 	set :emu_shared_media, "http://media.emu.im"
 	set :emu_share_gif_page, "http://play.emu.im/gif"
-	set :emu_img, "/emu-shares/img"
-	set :emu_css, "/emu-shares/css"
+	set :emu_xrc, "/emu-shares"
 	set :emu_app_page, "http://app.emu.im"
 	set :emu_fb_app_id, "410672082391445" 
 end
@@ -47,14 +43,14 @@ get '/gif/:oid' do
 	oid = params["oid"]
 
 	@oid = oid
-	@img = settings.emu_img
-	@css = settings.emu_css
+	@xrc = settings.emu_xrc
 	@shared_media = settings.emu_shared_media
 	@fb_app_id = settings.emu_fb_app_id
 	@gif_url = settings.emu_shared_media + "/" + oid + ".gif"
 	@page_url = settings.emu_share_gif_page + "/" + oid
 	@missing_gif_url = settings.emu_shared_media + "/" + "emu.gif"
 	@app_url = settings.emu_app_page
+	@mixpanel_token = settings.emumixpanel_token
 	
 	erb :emusharegif
 end
@@ -63,14 +59,14 @@ get '/giftest/:oid' do
 	oid = params["oid"]
 
 	@oid = oid
-	@img = settings.emutest_img
-	@css = settings.emutest_css
+	@xrc = settings.emutest_xrc
 	@shared_media = settings.emutest_shared_media
 	@fb_app_id = settings.emutest_fb_app_id
 	@gif_url = settings.emutest_shared_media + "/" + oid + ".gif"
 	@page_url = settings.emutest_share_gif_page + "/" + oid
 	@missing_gif_url = settings.emutest_shared_media + "/" + "emu.gif"
 	@app_url = settings.emutest_app_page
+	@mixpanel_token = settings.emumixpanel_token
 
 	erb :emusharegif
 end
---
+++ public/emu-shares/js/main.js
@@ -0,0 +1,58 @@
+
+function bind_gif_loading()
+{
+    $("#gif")
+    .load(function() { 
+        $('#shares-container').fadeIn();
+        $("#gif").fadeIn();
+    })
+    .error(function() {
+        var gif = $("#gif");
+        gif.unbind("load");
+        gif.unbind("error");
+        gif.attr("src", "<%= @missing_gif_url %>");
+        gif.fadeIn();
+        $('#shares-container').fadeOut();
+    });
+}
+
+
+function analytics(oid)
+{
+    var ref = getUrlParameter('r');
+    if (!ref) ref = 'undefined';
+    var name = getUrlParameter('n');
+    if (!name) name = 'undefined';
+
+    // Track the view of the page
+    mixpanel.track("Web:EmuSharedGifPage:View", {"Reference":ref, "name":name, "oid":oid});
+
+    // Track links to landing page
+    mixpanel.track_links("#header a", "Web:EmuSharedGifPage:LinkToLandingPage", {"Reference":ref, "name":name, "oid":oid, "sender":"header"});
+    mixpanel.track_links("#footer a", "Web:EmuSharedGifPage:LinkToLandingPage", {"Reference":ref, "name":name, "oid":oid, "sender":"footer"});
+    mixpanel.track_links("#emu", "Web:EmuSharedGifPage:LinkToLandingPage", {"Reference":ref, "name":name, "oid":oid, "sender":"emu"});
+
+    // Track share links
+    mixpanel.track_links("#share-facebook", "Web:EmuSharedGifPage:ShareClicked", {"Reference":ref, "name":name, "oid":oid, "shareMethod":"facebook"});
+    mixpanel.track_links("#share-pinterest", "Web:EmuSharedGifPage:ShareClicked", {"Reference":ref, "name":name, "oid":oid, "shareMethod":"pinterest"});
+    mixpanel.track_links("#share-tumblr", "Web:EmuSharedGifPage:ShareClicked", {"Reference":ref, "name":name, "oid":oid, "shareMethod":"tumblr"});
+    mixpanel.track_links("#share-reddit", "Web:EmuSharedGifPage:ShareClicked", {"Reference":ref, "name":name, "oid":oid, "shareMethod":"reddit"});
+}
+
+
+/** 
+    helper functions 
+**/
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+} 
